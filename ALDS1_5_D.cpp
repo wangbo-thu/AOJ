113ead1815c597def749d20bfc9ae1d67fca20be
xingyunbo <wangbo_zju@126.com>
2017-12-11 11:02:37 +0800

    20171211@lab
    一个上午，2个半小时，总算ac，慢慢来吧，下午写论文
    思维要全面

--- ALDS1_5_D.cpp
+++ ALDS1_5_D.cpp
@@ -1,40 +1,63 @@
 // ALDS1_5_D.cpp
 // Divide and Conquer - The Number of Inversions
-// Error: 省掉排序是不对的
+// 套用mergesort的程序，注意cnt的范围，int和long一样长都是2^31 -1，得用long long
 
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int CntOfInversion(vector<int>& nums, int left, int right)
+long long cnt = 0;
+const int SENTINEL = 1E9 + 5;
+void trace(vector<int>& nums)
 {
-	if (left + 1 >= right) return 0;
-	int cnt = 0;
-	int mid = left + (right - left) / 2;
-	int i = left;
-	int j = mid;
-	while (i < mid && j < right) {
-		if (nums[i] > nums[j]) {
-			cnt += mid - i;
-			j++;
+	for (int i = 0; i < nums.size(); i++) {
+		if (i > 0) cout << " ";
+		cout << nums[i];
+	}
+	cout << endl;
+}
+
+void merge(vector<int>& nums, int left, int mid, int right)
+{
+	vector<int> L(nums.begin() + left, nums.begin() + mid);
+	vector<int> R(nums.begin() + mid, nums.begin() + right);
+	L.push_back(SENTINEL);
+	R.push_back(SENTINEL);
+	int i = 0;
+	int j = 0;
+	for (int k = left; k < right; k++) {
+		if (L[i] <= R[j]) {
+			nums[k] = L[i++];
 		}
 		else {
-			i++;
+			nums[k] = R[j++];
+			cnt += L.size() - 1 - i;
 		}
 	}
-	return cnt + CntOfInversion(nums, left, mid) + CntOfInversion(nums, mid, right);
-
 }
+
+void MergeSort(vector<int>& nums, int left, int right)
+{
+	if (left + 1 >= right) return;
+	int mid = left + (right - left) / 2;
+	MergeSort(nums, left, mid);
+	MergeSort(nums, mid, right);
+	merge(nums, left, mid, right);
+}
+
+// FILE *stream1, *stream2;
 
 int main()
 {
+//	freopen_s(&stream1, "in.txt", "r", stdin);
+//	freopen_s(&stream2, "out.txt", "w", stdout);
 	int n;
 	cin >> n;
 	vector<int> nums(n);
 	for (int i = 0; i < n; i++) {
 		cin >> nums[i];
 	}
-	int res = CntOfInversion(nums, 0, n);
-	cout << res << endl;
+	MergeSort(nums, 0, n);
+//	trace(nums);
+	cout << cnt << endl;
 	return 0;
 }
