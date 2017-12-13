70416c29dc54c8422150888216036cf31e93e784
wangbo_thu <wangbo_thu@163.com>
2017-12-13 13:25:33 +0800

    20171213@dorm
    总算AC了，代码有些混乱，说明思路还不甚清楚。关于圆圈内计数结构上还得再斟酌

--- ALDS1_6_D.cpp
+++ ALDS1_6_D.cpp
@@ -8,38 +8,42 @@ using namespace std;
 const int MAXW = 10005;
 int main()
 {
-	int n;
-	cin >> n;
-	vector<int> w(n);
-	vector<int> w_sorted(MAXW, -1);
-	for (int i = 0; i < n; i++) {
-		cin >> w[i];
-		w_sorted[w[i]]++;
-	}
-	
-	int w_min = MAXW;	
-	int w_cnt = 0;
-	for (int i = 0; i < w_sorted.size(); i++) {
-		w_min = min(w_min, w[i]);
-		if (w_sorted[i] >= 0) {
-			w_sorted[i] += w_cnt;
-			w_cnt++;
-		}
-	}
+    int n;
+    cin >> n;
+    vector<int> w(n);
+    vector<int> w_sorted(MAXW, -1);
+    int w_min = MAXW;
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+        w_sorted[w[i]]++;
+        w_min = min(w_min, w[i]);
+    }
 
-	int w_sum = 0;
+    int w_cnt = 0;
+    for (int i = 0; i < w_sorted.size(); i++) {
+        if (w_sorted[i] >= 0) {
+            w_sorted[i] += w_cnt;
+            w_cnt++;
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		int w_cnt1 = 0;
-		int w_min_partial = MAXW;
-		for (int j = i; w_sorted[w[j]] != -1; j = w_sorted[w[j]], w_sorted[w[j]] = -1) {
-			w_min_partial = min(w_min_partial, w[j]);
-			w_cnt1++;
-			w_sum += w[j];
-		}
-		w_sum += min((w_cnt1 - 2) * w_min_partial, (w_cnt1 - 2) * w_min + 2 * (w_min_partial + w_min));
-	}
+    int w_sum = 0;
 
-	cout << res << endl;
-	return 0;
+    for (int i = 0; i < n; i++) {
+        if (w_sorted[w[i]] == -1) continue;
+        int w_cnt1 = 0;
+        int w_min_partial = w[i];
+        for (int j = i; w_sorted[w[j]] != -1; ) {
+            w_min_partial = min(w_min_partial, w[j]);
+            w_cnt1++;
+            w_sum += w[j];
+            int tmp = j;
+            j = w_sorted[w[j]];
+            w_sorted[w[tmp]] = -1;
+        }
+        w_sum += min((w_cnt1 - 2) * w_min_partial, (w_cnt1 + 1) * w_min + w_min_partial);
+    }
+
+    cout << w_sum << endl;
+    return 0;
 }
