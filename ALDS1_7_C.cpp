072da514914640fc6df006877b63f0965d3972a8
wangbo_thu <wangbo_thu@163.com>
2017-12-17 00:08:47 +0800

    20171217@dorm
    刚才提交的是错的

--- ALDS1_7_C.cpp
+++ ALDS1_7_C.cpp
@@ -7,71 +7,72 @@ using namespace std;
 
 struct Node
 {
-	int id;
-	int lchild;
-	int rchild;
-	int parent = -1;
+    int id;
+    int lchild;
+    int rchild;
+    int parent = -1;
 };
 
-void Preorder(Node& root)
+void Preorder(vector<Node> &nodes, int id)
 {
-	if (root.lchild == -1 && root.rchild == -1)
-		return;
-	cout << " " << root.id;
-	Preorder(root.lchild);
-	Preorder(root.rchild);
+    cout << " " << nodes[id].id;
+    if (nodes[id].lchild != -1)
+        Preorder(nodes, nodes[id].lchild);
+    if (nodes[id].rchild != -1)
+        Preorder(nodes, nodes[id].rchild);
 }
 
-void Inorder(Node& root)
+void Inorder(vector<Node> &nodes, int id)
 {
-	if (root.lchild == -1 && root.rchild == -1)
-		return;
-	Inorder(root.lchild);
-	cout << " " << root.id;
-	Inorder(root.rchild);
+    if (nodes[id].lchild != -1)
+        Inorder(nodes, nodes[id].lchild);
+    cout << " " << nodes[id].id;
+    if (nodes[id].rchild != -1)
+        Inorder(nodes, nodes[id].rchild);
 }
 
-void Postorder(Node& root)
+void Postorder(vector<Node> &nodes, int id)
 {
-	if (root.lchild == -1 && root.rchild == -1)
-		return;
-	Postorder(root.lchild);
-	Postorder(root.rchild);
-	cout << " " << root.id;
+    if (nodes[id].lchild != -1)
+        Postorder(nodes, nodes[id].lchild);
+    if (nodes[id].rchild != -1)
+        Postorder(nodes, nodes[id].rchild);
+    cout << " " << nodes[id].id;
 }
 
 int main()
 {
-	int n;
-	cin >> n;
-	vector<Node> nodes(n);
-	for (int i = 0; i < n; i++) {
-		int id, lc, rc;
-		cin >> id >> lc >> rc;
-		nodes[id].id = id;
-		nodes[id].lchild = lc;
-		nodes[id].rchild = rc;
-		if (lc != -1) {
-			nodes[lc].parent = id;
-		}
-		if (rc != -1) {
-			nodes[rc].parent = id;
-		}
-	}
-	
-	Node root;
-	for (int i = 0; i < n; i++) {
-		if (nodes[i].parent == -1) {
-			root = nodes[i];
-			break;
-		}
-	}
-	
-	cout << "Preorder" << endl;
-	Preorder(root);
-	cout << "Inorder" << endl;
-	Inorder(root);
-	cout << "Postorder" << endl;
-	Postorder(root);
-	return 0;
+    int n;
+    cin >> n;
+    vector<Node> nodes(n);
+    for (int i = 0; i < n; i++) {
+        int id, lc, rc;
+        cin >> id >> lc >> rc;
+        nodes[id].id = id;
+        nodes[id].lchild = lc;
+        nodes[id].rchild = rc;
+        if (lc != -1) {
+            nodes[lc].parent = id;
+        }
+        if (rc != -1) {
+            nodes[rc].parent = id;
+        }
+    }
+
+    int root_id;
+    for (int i = 0; i < n; i++) {
+        if (nodes[i].parent == -1) {
+            root_id = i;
+            break;
+        }
+    }
+
+    cout << "Preorder" << endl;
+    Preorder(nodes, root_id);
+    cout << endl << "Inorder" << endl;
+    Inorder(nodes, root_id);
+    cout << endl << "Postorder" << endl;
+    Postorder(nodes, root_id);
+    cout << endl;
+    return 0;
 }
