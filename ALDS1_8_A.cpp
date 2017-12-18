8d237227f2deac53285fb73fac2d1f9d9f5b1638
wangbo_thu <wangbo_thu@163.com>
2017-12-18 14:10:38 +0800

    20171218@dorm
    又提交错了，这一版是对的

--- ALDS1_8_A.cpp
+++ ALDS1_8_A.cpp
@@ -6,76 +6,76 @@
 using namespace std;
 const int NIL = -1;
 struct TreeNode {
-	int val;
-	TreeNode* lchild;
-	TreeNode* rchild;
-	//TreeNode* parent;
-	TreeNode(int x): val(x), lchild(NULL), rchild(NULL) {}
+    int val;
+    TreeNode* lchild;
+    TreeNode* rchild;
+    //TreeNode* parent;
+    TreeNode(int x): val(x), lchild(NULL), rchild(NULL) {}
 };
 
-void insert(TreeNode &tree, TreeNode &node)
+void insert(TreeNode *&tree, TreeNode *node)
 {
-	if (!tree) {
-		
-		TreeNode tmp = tree;
-		while (tree != NULL) {
-			tmp = tree;
-			if (node.val < tree.val) {
-				tree = tree->lchild;
-			}
-			else {
-				tree = tree->rchild;
-			}
-		} 
-		if (node.val < tmp.val) {
-			tmp->lchild = node;
-		}
-		else {
-			tmp->rchild = node;
-		}
-	}
-	else {
-		tree.val = node.val;
-	}
+    if (tree) {
+        TreeNode *tmp = tree;
+        TreeNode *tmp2 = tmp;
+        while (tmp != NULL) {
+            tmp2 = tmp;
+            if (node->val < tmp->val) {
+                tmp = tmp->lchild;
+            }
+            else {
+                tmp = tmp->rchild;
+            }
+        }
+        if (node->val < tmp2->val) {
+            tmp2->lchild = node;
+        }
+        else {
+            tmp2->rchild = node;
+        }
+    }
+    else {
+        tree = node;
+}
 }
 
-void inorder(TreeNode &tree) {
-	if (!tree) return;
-	inorder(tree->lchild);
-	cout << " " << tree->val;
-	inorder(tree->rchild);
+void inorder(TreeNode *tree) {
+    if (!tree) return;
+    inorder(tree->lchild);
+    cout << " " << tree->val;
+    inorder(tree->rchild);
 }
 
-void preorder(TreeNode &tree) {
-	if (!tree) return;
-	cout << " " << tree->val;
-	preorder(tree->lchild);
-	preorder(tree->rchild);
+void preorder(TreeNode *tree) {
+    if (!tree) return;
+    cout << " " << tree->val;
+    preorder(tree->lchild);
+    preorder(tree->rchild);
 }
 
-void print(TreeNode &tree) {
-	inorder(tree);
-	cout << endl;
-	preorder(tree);
-	cout << endl;
+void print(TreeNode *tree) {
+    inorder(tree);
+    cout << endl;
+    preorder(tree);
+    cout << endl;
 }
 
 int main()
 {
-	int n;
-	cin >> n;
-	string cmd;
-	int num;
-	TreeNode tree;
-	while(n--) {	
-		cin >> cmd;
-		if (cmd == "insert") {
-			cin >> num;
-			TreeNode node = TreeNode(num);
-			insert(tree, node);
-		}
-		if (cmd == "print")
-			print(tree);
-	}
-	return 0;
+    int n;
+    cin >> n;
+    string cmd;
+    int num;
+    TreeNode *tree = NULL;
+    while(n--) {
+        cin >> cmd;
+        if (cmd == "insert") {
+            cin >> num;
+            TreeNode *node = new TreeNode(num);
+            insert(tree, node);
+        }
+        if (cmd == "print")
+            print(tree);
+    }
+    return 0;
 }
